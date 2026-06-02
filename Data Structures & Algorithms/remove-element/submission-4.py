e17f8fcbbe32210638896b021d9fe51875586aab
Swarup Nandakumar <nswarup.sn@gmail.com>
2026-06-02 16:03:29 +0530

    Add: remove-element - submission-4

---
+++ Data Structures & Algorithms/remove-element/submission-4.py
@@ -0,0 +1,10 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        if not nums:
+            return 0
+        write_index = 0
+        for read_index in range(len(nums)):
+            if nums[read_index] != val:
+                nums[write_index] = nums[read_index]
+                write_index += 1
+        return write_index
