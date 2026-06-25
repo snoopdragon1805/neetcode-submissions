9d8d2cc7543c142f1b07877f35e3a87c09667ff8
Swarup Nandakumar <nswarup.sn@gmail.com>
2026-06-25 23:02:34 +0530

    Add: permutations-ii - submission-1

---
+++ Data Structures & Algorithms/permutations-ii/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        res=[]
+        nums.sort()
+        n = len(nums)
+        count = Counter(nums)
+        def backtrack(cur):
+            if len(cur) == n:
+                res.append(cur.copy())
+                return
+            for i in count:
+                if count[i]!=0:
+                    cur.append(i)
+                    count[i]-=1
+                    backtrack(cur)
+                    cur.pop()
+                    count[i]+=1
+            
+        backtrack([])
+        return res
